60f89f879f1cee43c19c6fdf218ad333c3feb916
Joane Alves <joanealves2011@gmail.com>
2022-03-21 15:30:33 -0300

    Route adjustment with react router dom and we create initial structure list and view components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import List from './pages/list/List'
+import View from './pages/view/View'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 
 export function App() {
   return (
     <div className="App">
-      <h1> Hello World</h1>
+       <Router>
+          <h1>
+            <Link to="/pokemons/list">Pokemon list</Link>
+          </h1>        
+          <Switch>
+            <Route path="/pokemons/list" component={List} />
+            <Route
+              path="/pokemons/:name"
+              component={View}
+            />
+            <Route path="/" exact>
+              <Redirect to="/pokemons/list" />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
