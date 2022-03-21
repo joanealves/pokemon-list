24678a59383d307282343b13ab393fefb47b6a6f
Joane Alves <joanealves2011@gmail.com>
2022-03-21 19:09:56 -0300

    Create html input and adjust use state

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -3,23 +3,36 @@ import { getAllPokemon } from '../../services/index'
 
 const List = () => {
     const [pokemonsData,setPokemonsData] = useState([]);
-    const [nestUrl, setNextUrl] = useState('');
+    const [nextUrl, setNextUrl] = useState('');
     const [prevUrl, setPrevUrl] = useState('');
     const [loading, setLoading] = useState(true);
     const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
-        teste()
+        fetchData()
     }, [])
 
-    const teste = async () => {
-        const user = await  getAllPokemon()
-        console.log(user)       
+    const fetchData = async () => {
+        const allPokemon = await  getAllPokemon()
+        console.log(allPokemon) 
+        setNextUrl(allPokemon.next)
+        setPrevUrl(allPokemon.previous)
+        setLoading(false)
     }
 
     return (
         <div>
             List
+            <input 
+                type="search"
+                value="Buscar"
+                onChange={(ev)=>setSearch(ev.target.value)}
+            />    
+            {loading ? <h1>Loadding...</h1> : (
+                <h1> Data is fetched </h1>
+            )}
+
         </div>
     );
 }
